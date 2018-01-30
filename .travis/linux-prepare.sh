d1d3d2c3308c597b19f3fc384fc2fd7e8e1184a3
Flavio Fernandes <flavio.fernandes@ibm.com>
2018-01-30 09:02:22 -0500

    travis: wait for /var/lib/dpkg/lock to be released
    https://api.travis-ci.org/v3/job/334549035/log.txt
    
    Could not get lock /var/lib/dpkg/lock - open (11: Resource temporarily unavailable)
    
    ...
    ./.travis/${TRAVIS_OS_NAME}-prepare.sh
    ~/build/flavio-fernandes/oclock/.travis ~/build/flavio-fernandes/oclock
    E: Could not get lock /var/lib/dpkg/lock - open (11: Resource temporarily unavailable)
    E: Unable to lock the administration directory (/var/lib/dpkg/), is another process using it?
    ...

--- .travis/linux-prepare.sh
+++ .travis/linux-prepare.sh
@@ -2,6 +2,13 @@
 
 set -o errexit
 
+# Apt update. Retry in case other apt process is taking place
+NEXT_WAIT_TIME=1
+until sudo apt-get update || [ $NEXT_WAIT_TIME -eq 66 ]; do
+    sleep $(( NEXT_WAIT_TIME++ ))
+    echo "Will retry apt update.... ${NEXT_WAIT_TIME}"
+done
+
 pushd $(dirname $0)
 for pkg in $(cat ../bindep.txt); do
     sudo apt-get install -y $pkg
