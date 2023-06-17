68ca8f76485929dbdd8fd3627911aef413deafb0
Flavio Fernandes <flavio@flaviof.com>
2023-06-17 13:31:06 -0400

    misc/weatherMsg.sh use open weather data

--- misc/weatherMsg.sh
+++ misc/weatherMsg.sh
@@ -28,58 +28,62 @@ function add_on_exit()
     fi
 }
 
-# "time": 1674300068, "summary": "Mostly Cloudy", "icon": "partly-cloudy-night", "nearestStormDistance": 21, "nearestStormDistance": 309, "precipIntensity": 0, "precipProbability": 0, "temperature": 27.62, "apparentTemperature": 27.91, "dewPoint": 25.67, "humidity": 0.92, "pressure": 1016.6, "windSpeed": 2.69, "windGust": 6.14, "windBearing": 352, "cloudCover": 0.82, "uvIndex": 0, "visibility": 10, "ozone": 381.8, "precipProbabilityPercent": "0+%25"
-
 function weather_details()
 {
     windSpeed='555'
-    precipProbabilityPercent='909+%25'
-    nearestStormDistance='321'
-    dewPoint='66.6'
+    windDeg='360'
+    windGust='666'
+    dewPoint='66.66'
+    cloudCover='100'
+    pressure='1001'
     summary='chance of meatballs'
-    cloudCover='1.01'
-    uvIndex='42'
     dawn='9:99'
     dusk='23:99'
     tempMin='1.1'
     tempMax='99.9'
 
-    # override the fudged values above by reading in /tmp/weather.txt
-    [ -e /tmp/weather.txt ] && source /tmp/weather.txt
+    # override the fudged values above by reading in /dev/shm/weather.txt
+    [ -e /dev/shm/weather.txt ] && source /dev/shm/weather.txt
 
-    curl --request POST  ${urlMsgMode} --data "msg=${summary}&repeats=4&timeout=0&y=0&font=4&color=2"
-
-    curl --request POST  ${urlMsgBg}  --data "msg=Wind&x=0&y=1&index=0&color=1&font=0&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${windSpeed}&x=21&y=1&index=1&color=0&font=0&enabled=yes"
-
-    curl --request POST  ${urlMsgBg}  --data "msg=Precip&x=42&y=1&index=2&color=1&font=0&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${precipProbabilityPercent}&x=68&y=1&index=3&color=0&font=0&enabled=yes"
-
-    curl --request POST  ${urlMsgBg}  --data "msg=Dist&x=95&y=1&index=4&color=1&font=0&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${nearestStormDistance}&x=113&y=1&index=5&color=0&font=0&enabled=yes"
-
-    curl --request POST  ${urlMsgBg}  --data "msg=Dew&x=0&y=9&index=6&color=1&font=0&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${dewPoint}&x=15&y=9&index=7&color=0&font=0&enabled=yes"
-
-    curl --request POST  ${urlMsgBg}  --data "msg=Cloud&x=42&y=9&index=10&color=1&font=0&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${cloudCover}&x=66&y=9&index=11&color=0&font=0&enabled=yes"
-
-    curl --request POST  ${urlMsgBg}  --data "msg=UV&x=83&y=9&index=12&color=1&font=0&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${uvIndex}&x=95&y=9&index=13&color=0&font=0&enabled=yes"
+    LONE=1
+    LTWO=9
 
     # dusk, dawn, tempMin, tempMax animation
 
-    curl --request POST  ${urlMsgBg}  --data "msg=dawn&index=14&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=0&color=2&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${dawn}&index=15&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=1&color=2&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=dawn&index=0&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=0&color=2&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${dawn}&index=1&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=1&color=2&enabled=yes"
 
-    curl --request POST  ${urlMsgBg}  --data "msg=dusk&index=16&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=2&color=1&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${dusk}&index=17&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=3&color=1&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=dusk&index=2&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=2&color=1&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${dusk}&index=3&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=3&color=1&enabled=yes"
 
-    curl --request POST  ${urlMsgBg}  --data "msg=Min T&index=18&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=4&color=2&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${tempMin}&index=19&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=5&color=2&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=Min T&index=4&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=4&color=2&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${tempMin}&index=5&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=5&color=2&enabled=yes"
 
-    curl --request POST  ${urlMsgBg}  --data "msg=Max T&index=20&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=6&color=1&enabled=yes"
-    curl --request POST  ${urlMsgBg}  --data "msg=${tempMax}&index=21&x=107&y=9&animationStep=5&animationPhase=8&animationPhaseValue=7&color=1&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=Max T&index=6&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=6&color=1&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${tempMax}&index=7&x=107&y=${LTWO}&animationStep=5&animationPhase=8&animationPhaseValue=7&color=1&enabled=yes"
+
+    # non animated info
+ 
+    curl --request POST  ${urlMsgBg}  --data "msg=Wind&x=0&y=${LONE}&index=8&color=1&font=0&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${windSpeed}&x=21&y=${LONE}&index=9&color=0&font=0&enabled=yes"
+
+    curl --request POST  ${urlMsgBg}  --data "msg=Gust&x=0&y=${LTWO}&index=10&color=1&font=0&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${windGust}&x=21&y=${LTWO}&index=11&color=0&font=0&enabled=yes"
+
+    curl --request POST  ${urlMsgBg}  --data "msg=Deg&x=36&y=${LONE}&index=12&color=1&font=0&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${windDeg}&x=50&y=${LONE}&index=13&color=0&font=0&enabled=yes"
+
+    curl --request POST  ${urlMsgBg}  --data "msg=Dew&x=36&y=${LTWO}&index=14&color=1&font=0&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${dewPoint}&x=51&y=${LTWO}&index=15&color=0&font=0&enabled=yes"
+
+    curl --request POST  ${urlMsgBg}  --data "msg=Pressure&x=69&y=${LONE}&index=16&color=1&font=0&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${pressure}&x=107&y=${LONE}&index=17&color=0&font=0&enabled=yes"
+    
+    curl --request POST  ${urlMsgBg}  --data "msg=Clud&x=74&y=${LTWO}&index=18&color=1&font=0&enabled=yes"
+    curl --request POST  ${urlMsgBg}  --data "msg=${cloudCover}&x=93&y=${LTWO}&index=19&color=0&font=0&enabled=yes"
+
+    # at last, start message mode now that all wanted msg bg is populated
+    curl --request POST  ${urlMsgMode} --data "msg=${summary}&repeats=4&timeout=0&y=0&font=4&color=2"
 }
 
 function display()
@@ -98,10 +102,10 @@ function display()
 
 # if invoked with parameters, add them to data file
 if [ $# -ne 0 ]; then \
-    exec 100>/tmp/weatherLock.lock || { >&2 echo "unable to open lock file" ; exit 1; }
+    exec 100>/dev/shm/weatherLock.lock || { >&2 echo "unable to open lock file" ; exit 1; }
     flock -w 10 100 || { >&2 echo "failed to lock lock file"; exit 2; }
 
-    [ -e /tmp/weather.txt ] && cat /tmp/weather.txt > /dev/shm/weather.txt.tmp || touch /dev/shm/weather.txt.tmp
+    [ -e /dev/shm/weather.txt ] && cat /dev/shm/weather.txt > /dev/shm/weather.txt.tmp || touch /dev/shm/weather.txt.tmp
     while (( "$#" )); do
         key="$1"
         value="$2"
@@ -112,7 +116,7 @@ if [ $# -ne 0 ]; then \
         cat /dev/shm/weather.txt.tmp.2 > /dev/shm/weather.txt.tmp
     done
     rm -f /dev/shm/weather.txt.tmp.2
-    mv /dev/shm/weather.txt.tmp /tmp/weather.txt
+    mv /dev/shm/weather.txt.tmp /dev/shm/weather.txt
 
     exit 0
 fi
